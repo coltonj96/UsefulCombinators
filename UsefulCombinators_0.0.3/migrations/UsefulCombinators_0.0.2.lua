02746e41736595f260c328e6489733ad00f1ae0e
coltonj96 <coltonjgriswold@aim.com>
2017-03-01 21:49:30 -0600

    Add files via upload

--- UsefulCombinators_0.0.3/migrations/UsefulCombinators_0.0.2.lua
+++ UsefulCombinators_0.0.3/migrations/UsefulCombinators_0.0.2.lua
@@ -1,10 +1,11 @@
 game.reload_script()
 
 for index, force in pairs(game.forces) do
-  force.reset_recipes()
   force.reset_technologies()
   if force.technologies["useful-combinators"].researched then
     force.recipes["random-combinator"].enabled = true
     force.recipes["comparator-combinator"].enabled = true
+    force.recipes["timer-combinator"].enabled = true
+    force.recipes["counting-combinator"].enabled = true
   end
 end
