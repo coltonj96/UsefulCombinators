3b4bff3fb975d47964a0b48e1a78b2d248867aa2
coltonj96 <coltonjgriswold@aim.com>
2017-03-01 21:41:56 -0600

    Add files via upload

---
+++ UsefulCombinators_0.0.3/prototypes/technology/technology.lua
@@ -0,0 +1,42 @@
+data:extend({
+  {
+    type = "technology",
+    name = "useful-combinators",
+    icon = "__UsefulCombinators__/graphics/technology/clock.png",
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "timer-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "counting-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "random-combinator"
+      }--[[,
+      {
+        type = "unlock-recipe",
+        recipe = "logic-combinator"
+      }]],
+      {
+        type = "unlock-recipe",
+        recipe = "comparator-combinator"
+      }
+    },
+    prerequisites = {"circuit-network"},
+    unit =
+    {
+      count = 100,
+      ingredients =
+      {
+        {"science-pack-1", 1},
+        {"science-pack-2", 1}
+      },
+      time = 15
+    },
+    order = "a-d-d"
+  }
+})
