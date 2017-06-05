534b4f312be592748148e036f59079c9bf859dd6
coltonj96 <coltonjgriswold@aim.com>
2017-06-04 19:22:00 -0500

    Add files via upload

---
+++ UsefulCombinators_0.1.8/prototypes/signal/signal.lua
@@ -0,0 +1,129 @@
+data:extend(
+{
+  {
+    type = "virtual-signal",
+    name = "counting-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/counting-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[counting]"
+  },
+  {
+    type = "virtual-signal",
+    name = "time-mod-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/time-mod-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[time-mod]"
+  },
+  {
+    type = "virtual-signal",
+    name = "divisor-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/divisor-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[divisor]"
+  },
+  {
+    type = "virtual-signal",
+    name = "interval-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/interval-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[interval]"
+  },
+  {
+    type = "virtual-signal",
+    name = "lower-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/lower-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[lower]"
+  },
+  {
+    type = "virtual-signal",
+    name = "plus-one-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/plus-one-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[plus-one]"
+  },
+  {
+    type = "virtual-signal",
+    name = "output-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/output-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[output]"
+  },
+  {
+    type = "virtual-signal",
+    name = "reset-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/reset-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[reset]"
+  },
+  {
+    type = "virtual-signal",
+    name = "ticks-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/ticks-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[ticks]"
+  },
+  {
+    type = "virtual-signal",
+    name = "upper-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/upper-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[upper]"
+  },
+  {
+    type = "virtual-signal",
+    name = "lt-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/lt-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[lt]"
+  },
+  {
+    type = "virtual-signal",
+    name = "lte-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/lte-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[lte]"
+  },
+  {
+    type = "virtual-signal",
+    name = "eq-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/eq-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[eq]"
+  },
+  {
+    type = "virtual-signal",
+    name = "gte-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/gte-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[gte]"
+  },
+  {
+    type = "virtual-signal",
+    name = "gt-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/gt-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[gt]"
+  },
+  {
+    type = "virtual-signal",
+    name = "blank",
+    icon = "__UsefulCombinators__/graphics/icons/signal/blank.png",
+    subgroup = "virtual-signal",
+    order = "u[blank]"
+  },
+  {
+    type = "virtual-signal",
+    name = "minus-one-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/minus-one-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[minus-one]"
+  },
+  {
+    type = "virtual-signal",
+    name = "radius-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/radius-signal.png",
+    subgroup = "virtual-signal",
+    order = "u[radius]"
+  }
+})
