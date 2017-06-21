5758b8361388db125d8f317d5a312789c1c0350f
coltonj96 <coltonjgriswold@aim.com>
2017-06-20 20:03:21 -0500

    Add files via upload

---
+++ UsefulCombinators_0.2.3/prototypes/recipe/recipes.lua
@@ -0,0 +1,200 @@
+data:extend({
+  {
+    type = "recipe",
+    name = "timer-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"arithmetic-combinator", 1},
+      {"constant-combinator", 1}
+    },
+    result = "timer-combinator"
+  },
+  {
+    type = "recipe",
+    name = "counting-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"arithmetic-combinator", 1},
+      {"decider-combinator", 1}
+    },
+    result = "counting-combinator"
+  },
+  {
+    type = "recipe",
+    name = "random-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 1},
+      {"constant-combinator", 1}
+    },
+    result = "random-combinator"
+  },
+  {
+    type = "recipe",
+    name = "comparator-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"constant-combinator", 1},
+      {"decider-combinator", 1}
+    },
+    result = "comparator-combinator"
+  },
+  {
+    type = "recipe",
+    name = "converter-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"constant-combinator", 1},
+      {"electronic-circuit", 2}
+    },
+    result = "converter-combinator"
+  },
+  {
+    type = "recipe",
+    name = "min-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 1},
+      {"decider-combinator", 1}
+    },
+    result = "min-combinator"
+  },
+  {
+    type = "recipe",
+    name = "max-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 1},
+      {"decider-combinator", 1}
+    },
+    result = "max-combinator"
+  },
+  {
+    type = "recipe",
+    name = "and-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "and-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "or-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "or-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "not-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "not-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "nand-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "nand-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "nor-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "nor-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "xor-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "xor-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "xnor-gate-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"copper-cable", 2}
+    },
+    result = "xnor-gate-combinator"
+  },
+  {
+    type = "recipe",
+    name = "detector-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 2},
+      {"decider-combinator", 1}
+    },
+    result = "detector-combinator"
+  },
+  {
+    type = "recipe",
+    name = "sensor-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 2},
+      {"decider-combinator", 1}
+    },
+    result = "sensor-combinator"
+  },
+  {
+    type = "recipe",
+    name = "railway-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"advanced-circuit", 2},
+      {"decider-combinator", 1}
+    },
+    result = "railway-combinator"
+  },
+  {
+    type = "recipe",
+    name = "color-combinator",
+    enabled = "false",
+    ingredients =
+    {
+      {"electronic-circuit", 1},
+      {"small-lamp", 1}
+    },
+    result = "color-combinator"
+  }
+})
