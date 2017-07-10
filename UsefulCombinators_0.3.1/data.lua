c0e574ddbf5a00b6d371c9e3486893c1ca51c086
coltonj96 <coltonjgriswold@aim.com>
2017-07-10 08:47:42 -0500

    Add files via upload

---
+++ UsefulCombinators_0.3.1/data.lua
@@ -0,0 +1,16 @@
+require("prototypes.categories.usefulcombinators")
+require("prototypes.categories.changes")
+require("prototypes.entity.entities")
+require("prototypes.item.items")
+require("prototypes.signal.signal")
+require("prototypes.recipe.recipes")
+require("prototypes.technology.technology")
+require("prototypes.styles.default")
+
+data:extend({
+  {
+    type = "custom-input",
+    name = "uc-key",
+    key_sequence = "SHIFT + Z",
+  },
+})
---
+++ UsefulCombinators_0.3.1/prototypes/technology/technology.lua
@@ -0,0 +1,103 @@
+data:extend({
+  {
+    type = "technology",
+    name = "useful-combinators",
+    icon = "__UsefulCombinators__/graphics/technology/clock.png",
+    icon_size = 64,
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "timer-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "counting-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "random-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "converter-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "min-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "max-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "and-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "nand-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "nor-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "not-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "or-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "xnor-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "xor-gate-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "detector-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "sensor-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "railway-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "color-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "emitter-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "receiver-combinator"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "power-combinator"
+      }
+    },
+    prerequisites = {"circuit-network"},
+    unit =
+    {
+      count = 100,
+      ingredients =
+      {
+        {"science-pack-1", 1},
+        {"science-pack-2", 1}
+      },
+      time = 15
+    },
+    order = "a-d-d"
+  }
+})
