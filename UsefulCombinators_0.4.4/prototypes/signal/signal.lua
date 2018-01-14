1806462b5a422ac42a321036cd746c2a7b580638
coltonj96 <coltonjgriswold@aim.com>
2018-01-14 18:04:47 +0200

    Add files via upload

---
+++ UsefulCombinators_0.4.4/prototypes/signal/signal.lua
@@ -0,0 +1,179 @@
+  data:extend(
+{
+  {
+    type = "virtual-signal",
+    name = "counting-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/counting-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[counting]"
+  },
+  {
+    type = "virtual-signal",
+    name = "plus-one-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/plus-one-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[plus-one]"
+  },
+  {
+    type = "virtual-signal",
+    name = "output-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/output-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[output]"
+  },
+  {
+    type = "virtual-signal",
+    name = "reset-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/reset-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[reset]"
+  },
+  {
+    type = "virtual-signal",
+    name = "ticks-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/ticks-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[ticks]"
+  },
+  {
+    type = "virtual-signal",
+    name = "blank",
+    icon = "__UsefulCombinators__/graphics/icons/signal/blank.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[blank]"
+  },
+  {
+    type = "virtual-signal",
+    name = "minus-one-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/minus-one-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[minus-one]"
+  },
+  {
+    type = "virtual-signal",
+    name = "radius-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/radius-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[radius]"
+  },
+  {
+    type = "virtual-signal",
+    name = "red-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/red-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[red]"
+  },
+  {
+    type = "virtual-signal",
+    name = "yellow-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/yellow-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[yellow]"
+  },
+  {
+    type = "virtual-signal",
+    name = "green-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/green-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[green]"
+  },
+  {
+    type = "virtual-signal",
+    name = "cyan-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/cyan-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[cyan]"
+  },
+  {
+    type = "virtual-signal",
+    name = "blue-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/blue-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[blue]"
+  },
+  {
+    type = "virtual-signal",
+    name = "magenta-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/magenta-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[magenta]"
+  },
+  {
+    type = "virtual-signal",
+    name = "white-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/white-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[white]"
+  },
+  {
+    type = "virtual-signal",
+    name = "watts-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/watts-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[watts]"
+  },
+  {
+    type = "virtual-signal",
+    name = "kilo-watts-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/kilo-watts-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[kilo]"
+  },
+  {
+    type = "virtual-signal",
+    name = "mega-watts-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/mega-watts-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[mega]"
+  },
+  {
+    type = "virtual-signal",
+    name = "giga-watts-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/giga-watts-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[giga]"
+  },
+  {
+    type = "virtual-signal",
+    name = "input-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/input-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[input]"
+  },
+  {
+    type = "virtual-signal",
+    name = "start-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/start-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[start]"
+  },
+  {
+    type = "virtual-signal",
+    name = "stop-signal",
+    icon = "__UsefulCombinators__/graphics/icons/signal/stop-signal.png",
+		icon_size = 32,
+    subgroup = "virtual-signal",
+    order = "u[stop]"
+  }
+})
